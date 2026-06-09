0849c5dc7fea066846e454d0079c4fe3a4a3299e
Marco Geweke <marco.geweke@otto.de>
2026-06-09 09:58:42 +0200

    Convert library to Spring Boot auto-configuration
    Co-authored-by: Tjark Fischer <tjark.fischer@otto.de>

--- src/main/java/de/otto/esidialect/EsiDialectConfiguration.java
+++ src/main/java/de/otto/esidialect/EsiDialectConfiguration.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.processor.element.AbstractElementTagProcessor;
 
 /**
@@ -25,7 +25,7 @@ import org.thymeleaf.processor.element.AbstractElementTagProcessor;
  * A user of this library is free to override this bean definition or provide a different implementation for the fetch-function.
  * </p>
  */
-@Configuration
+@AutoConfiguration
 @EnableConfigurationProperties(EsiDialectProperties.class)
 @ConditionalOnProperty(prefix = "esiinclude-thymeleaf-dialect",
         value = "dialect-enabled", havingValue = "true")
--- src/main/java/de/otto/esidialect/LocalhostProxyConfiguration.java
+++ src/main/java/de/otto/esidialect/LocalhostProxyConfiguration.java
@@ -1,16 +1,16 @@
 package de.otto.esidialect;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * <p>Defines an HTTP proxy that is used to retrieve secondary includes if desired: Esi-included pages might
  * include resources like CSS and JavaScript files causing CORS errors. Therefore, this proxy sets the HTTP
  * header <code>Access-Control-Allow-Origin: *</code> on all responses.</p>
  */
-@Configuration
+@AutoConfiguration
 @EnableConfigurationProperties(EsiDialectProperties.class)
 @ConditionalOnProperty(prefix = "esiinclude-thymeleaf-dialect", value = "proxy-enabled", havingValue = "true")
 public class LocalhostProxyConfiguration {
